9323628ba7c77c747a1cd3ec4eef9764ddd5067a
Лариса Шашук <larysashashuk@Larisas-Mac-mini.local>
2021-09-13 17:16:17 +0300

    2test

--- src/components/ProceduresGalleryPage/ProceduresGalleryPage.js
+++ src/components/ProceduresGalleryPage/ProceduresGalleryPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './ProceduresGalleryPage.module.scss';
 
 export default function ProceduresGalleryPage(){
-    return <div>
+    return <div className={styles.container}> 
         <div>
-            test
+            
         </div>
     </div>
 }
