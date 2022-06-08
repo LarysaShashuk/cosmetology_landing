b5c116dd85efd7ce2b0be1c7475c0dab9ab02fcc
Larysa <larisa.shashuk@gmail.com>
2022-06-08 23:32:32 +0300

    sale information

--- src/data/SaleInformation.js
+++ src/data/SaleInformation.js
@@ -1,7 +1,7 @@
 const SALE_INFORMATION = {
   title: 'Даруємо мікродермабразію',
   subtitle: 'до кожної процедури чистки обличчя в подарунок!',
-  endTime: '*до 31.05.2022',
+  endTime: '*до 30.06.2022',
   img: '/assets/images/sale-2.jpeg',
 };
 
