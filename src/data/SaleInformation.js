ebf80a430cde397a6d1cc3e98d23423c3fceb6f4
spaindoc <111993357+spaindoc@users.noreply.github.com>
2022-08-24 17:10:28 +0300

    Update SaleInformation.js

--- src/data/SaleInformation.js
+++ src/data/SaleInformation.js
@@ -1,7 +1,7 @@
 const SALE_INFORMATION = {
   title: 'Даруємо мікродермабразію',
   subtitle: 'до кожної процедури чистки обличчя в подарунок!',
-  endTime: '*до 30.06.2022',
+  endTime: '',
   img: '/assets/images/sale-2.jpeg',
 };
 
